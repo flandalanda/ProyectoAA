49dff804a5621018c7b9c0c4e2b46ee8724cabaf
Luis Felipe Landa Lizarralde <flandalanda@gmail.com>
2020-04-04 21:14:47 -0600

    Added clear and clc to script Ej2_3.

--- Ej2_3.m
+++ Ej2_3.m
@@ -1,4 +1,5 @@
 % Script to perform excercise 2.3
+clear;clc;
 
 % We define a handle for the Beale function
 f = @(x) (1.5 - x(1) + x(1)*x(2))^2 + (2.25 - x(1) + x(1)*x(2)^2)^2 + (2.625 - x(1) + x(1)*x(2)^3)^2;

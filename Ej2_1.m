86a494e84104d3b04ee14185f281d7924f72e6f7
MarcoMedS <62576658+MarcoMedS@users.noreply.github.com>
2020-04-04 22:47:09 -0600

    Update Ej2_1.m
    Added comments to Ej2_1 script

--- Ej2_1.m
+++ Ej2_1.m
@@ -1,16 +1,15 @@
 clear;   close all;   clc;
 
-%% Define f with two arguments  and  so that
-% it can be evaluated for matrices of values X, Y
+%% Define the Beale function to be evaluated at a vector x
 f = @(x) (1.5 - x(1) + x(1).*x(2)).^2 + (2.25 - x(1) + x(1).*x(2).^2).^2 + (2.625 - x(1) + x(1).*x(2).^3).^2;
 
 
-%% define point and trust region radius
+%% Define point and trust region radius
 x0    = [0;0];
 delta = 1;
 
 
-%% plot f in cartesian coordinates arround x0
+%% Plot f in cartesian coordinates around x0
 showPlot = true;
 if showPlot
 	Delta = 1.1*delta;
@@ -22,17 +21,17 @@ if showPlot
 end
 
 
-%% plot quadratic model in trust region with polar coordinates arround x0
+%% Plot quadratic model in the trust region with polar coordinates around x0
 hold on
-% polar coordinates arround x0
+% Polar coordinates arround x0
 [T,R] = meshgrid(linspace(0,2*pi,64),linspace(0,delta,16));
 X     = R.*cos(T) +x0(1);
 Y     = R.*sin(T) +x0(2);
-% the quadratic model (simple in this case)
+% The quadratic model
 B = apHess(f,x0);
 g = apGrad(f,x0);
 m  = @(x) f(x0) + g'*x' + x*B*x';
-% evaluation and plot
+% Evaluation and plot
 Z  = arrayfun(@(x1,x2) m([x1,x2]), X, Y);
 s1 = mesh(X,Y,Z);
 view(130, 15)

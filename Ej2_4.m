4b7737fdb6b919f230a54556098f7c02efdf3403
Rafael <r4f4al@gmail.com>
2020-04-08 12:06:08 -0500

    actual

--- Ej2_4.m
+++ Ej2_4.m
@@ -1,25 +1,22 @@
-clear;   close all;   clc;
-
-
-f = @(x) (1.5 - x(1) + x(1)*x(2))^2 + (2.25 - x(1) + x(1)*x(2)^2)^2 + (2.625 - x(1) + x(1)*x(2)^3)^2;
-
-
 % Script to perform excercise 2.4
 clear;clc;close all;
 
 % We define a handle for the Beale function
 f = @(x) (1.5 - x(1) + x(1)*x(2))^2 + (2.25 - x(1) + x(1)*x(2)^2)^2 + (2.625 - x(1) + x(1)*x(2)^3)^2;
-stepsize = 0.01;
-[X,Y] = meshgrid(-3:stepsize:3);
-z = f(X,Y);
-niveles = [0.1, 3:5];
-contour(X,Y,z, niveles)
+
+% Definimos el punto inicial, el radio de la Region de Confianza y las
+% variables de entrada
+x0 = [-3; -3];
+xstar = [3;0.5];
+delta = 0.5;
+itmax = 1000;
+
+%Definimos los conjuntos de nivel
+stepsize =  0.01;
+[X,Y] = meshgrid(0:stepsize:5);
+Z = f(X,Y);
+niveles = [0.1, 1:10];
+contour(X,Y,Z, niveles)
 
 axis equal
-
-% example iteration history
 hold on
-x = [2:-0.2:0];
-y = x/2;
-plot(x,y,'--d')
-hold off
